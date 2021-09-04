b5bb8ac3a3a6bad5c99b7251dc2ae559ac74411d
RusAlex91 <aocrusalex91@yandex.ru>
2021-09-04 17:10:14 +0300

    fix path

--- dist/data.js
+++ dist/data.js
@@ -7,7 +7,7 @@ const portfolioCases = {
         'Простой музыкальный плеер. Использованные технологии: HTML, CSS, JS',
       github: 'https://github.com/RusAlex91/1.-Register-Validator',
       src: '../assets/portfolio/light/testgif2.gif',
-      webLink: 'projects/lite/retro-player/dist'
+      webLink: 'https://rusalex91.github.io/projects/lite/retro-player/dist/'
     },
     2: {
       id: 2,
