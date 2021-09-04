80dcf255d3a64bc2c807e5320ab84243362731f8
RusAlex91 <aocrusalex91@yandex.ru>
2021-09-04 19:57:47 +0300

    fix

--- dist/data.js
+++ dist/data.js
@@ -73,7 +73,7 @@ const portfolioCases = {
         'Валидация формы регистрации. Использованные технологии: HTML, CSS, JS',
       github: 'https://github.com/RusAlex91/1.-Register-Validator',
       src: '../assets/portfolio/light/test.png',
-      webLink: 'https://rusalex91.github.io/projects/heavy/find-a-god/guru'
+      webLink: 'https://rusalex91.github.io/projects/heavy/find-a-god/'
     },
     2: {
       id: 2,
